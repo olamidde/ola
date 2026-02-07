3310a50f23210063f1af134a37bc04cad2ce1ffd
Ola Ogunsanya <olamideogunsanya@outlook.com>
2026-02-07 09:46:40 -0500

    fix_compilation_error

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -34,7 +34,7 @@ function CardTitle({ className, ...props }: React.ComponentProps<"div">) {
       data-slot="card-title"
       className={cn("leading-none font-semibold", className)}
       {...props}
-    />j
+    />
   )
 }
 
